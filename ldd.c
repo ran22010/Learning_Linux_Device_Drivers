ef3a0c641483c6566e98a954617d5d06d1922d02
Raul Nair <raul.nair@uconn.edu>
2026-07-19 15:34:15 -0400

    Played around with proc file system

--- ldd.c
+++ ldd.c
@@ -1,17 +1,35 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL"); //*
 MODULE_AUTHOR("Raul"); //!
 MODULE_DESCRIPTION("Our first dynamically loadable module"); //!
 
+static struct proc_dir_entry *custom_proc_node;
+struct proc_ops custom_proc_ops = {
+
+};
+
 static int first_module_init (void) {
-    printk("Hello, World! (from Raul!!)\n");
+    printk("first_module_init: entry\n");
+
+    custom_proc_node = proc_create("ldd_driver",
+                                0,
+                                NULL,
+                                &custom_proc_ops);
+    
+    printk("first_module_init: exit\n");
+
     return 0;
 }
 
 static void first_module_exit (void) {
-    printk("Good, Bye! (from Raul!!)\n");
+    printk("first_module_exit: entry\n");
+
+    proc_remove(custom_proc_node);
+
+    printk("first_module_exit: exit\n");
 }
 
 module_init(first_module_init);
